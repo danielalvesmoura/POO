55af04bf6e1d4703c6a657d39c2fee897d0a859e
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-17 20:01:08 -0300

    Delete jogos directory

--- jogos/src/Global/Basico.java
+++
@@ -1,9 +0,0 @@
-package Global;
-
-import java.util.Random;
-import java.util.Scanner;
-
-public class Basico {
-    public Random random = new Random();
-    public Scanner input = new Scanner(System.in);
-}
