c1e2f349f065228ae2c8870c8cd306c812f38afe
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-26 21:10:53 -0300

    Update Main.java

--- Jogos 2.0/Main.java
+++ Jogos 2.0/Main.java
@@ -1,1 +1,14 @@
+public class Main {
 
+    public static Palavra palavra = new Palavra();
+
+    public static void main(String[] args) {
+
+        Anagrama anagrama = new Anagrama();
+        anagrama.jogo();
+
+    }
+
+
+
+}
