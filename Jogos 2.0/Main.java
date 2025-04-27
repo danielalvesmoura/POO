4453c05975e4d09a9ae06ca629a77e6a33d69d58
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-26 21:39:30 -0300

    Update Main.java

--- Jogos 2.0/Main.java
+++ Jogos 2.0/Main.java
@@ -1,11 +1,31 @@
 public class Main {
 
+    private static Global global = new Global();
     public static Palavra palavra = new Palavra();
+    public static Pontuacao pontuacao = new Pontuacao();
+
+    private static Boolean jogando = true;
 
     public static void main(String[] args) {
+        while(jogando) {
 
-        Anagrama anagrama = new Anagrama();
-        anagrama.jogo();
+            acao();
+
+        }
+
+    }
+
+    private static void acao() {
+        System.out.println("\n[1] Anagrama, [2] Caça Palavras, [3] Mudar Dificuldade, [4] Parar");
+        int respostaAcao = global.scanner.nextInt();
+
+        if (respostaAcao == 1) {
+            Anagrama anagrama = new Anagrama();
+            anagrama.jogo();
+        } else if (respostaAcao == 4) {
+            jogando = false;
+        }
+
 
     }
 
