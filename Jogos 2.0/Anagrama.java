353438e2f7496c5650426f5d865841fc9090e134
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-26 21:11:16 -0300

    Create Anagrama.java

---
+++ Jogos 2.0/Anagrama.java
@@ -0,0 +1,30 @@
+public class Anagrama {
+
+    Global global = new Global();
+
+
+    public void jogo() {
+        String palavraEmbaralhada = embaralhaPalavra(Main.palavra.palavraEscolhida);
+
+        System.out.println(palavraEmbaralhada);
+
+        Main.palavra.resposta = global.scanner.nextLine();
+
+    }
+
+    public String embaralhaPalavra(String palavra) {
+        char[] palavraArray = palavra.toCharArray();
+
+        for (int i = 0; i < palavraArray.length; i++) {
+            int posicaoAleatoria = global.inteiroAleatorio(0, palavraArray.length);
+
+            char aux = palavraArray[i];
+            palavraArray[i] = palavraArray[posicaoAleatoria];
+            palavraArray[posicaoAleatoria] = aux;
+        }
+
+
+        return new String(palavraArray);
+    }
+
+}
