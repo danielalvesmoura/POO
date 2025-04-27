bd3eeac776d5d76330423009243b890ce3227e2f
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-26 22:53:29 -0300

    Update Main.java

--- Jogos 2.0/Main.java
+++ Jogos 2.0/Main.java
@@ -3,14 +3,14 @@ public class Main {
     private static Global global = new Global();
     public static Palavra palavra = new Palavra();
     public static Pontuacao pontuacao = new Pontuacao();
+    public static Dica dica = new Dica();
 
     private static Boolean jogando = true;
 
     public static void main(String[] args) {
+
         while(jogando) {
-
             acao();
-
         }
 
     }
@@ -22,10 +22,23 @@ public class Main {
         if (respostaAcao == 1) {
             Anagrama anagrama = new Anagrama();
             anagrama.jogo();
+        }  else if (respostaAcao == 2) {
+            CacaPalavras cacaPalavras = new CacaPalavras();
+            cacaPalavras.jogo();
+        }  else if (respostaAcao == 3) {
+            mudarDificuldade();
         } else if (respostaAcao == 4) {
             jogando = false;
         }
 
+
+    }
+
+    private static void mudarDificuldade() {
+
+        System.out.println("Digite a quantidade desejada de letras [4-6]:");
+
+        Main.palavra.dificuldade = global.scanner.nextInt();
 
     }
 
