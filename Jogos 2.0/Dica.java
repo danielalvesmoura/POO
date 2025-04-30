b0f614f6a1ea9ad43d8d4912f88a5cb99ea29130
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-30 09:16:19 -0300

    Update Dica.java

--- Jogos 2.0/Dica.java
+++ Jogos 2.0/Dica.java
@@ -2,10 +2,10 @@ public class Dica {
 
     Boolean pediuDica = false;
 
-    public void dica() {
+    public void dica(int qualPalavraDica) {
         pediuDica = true;
 
-        char[] palavraArray = Main.palavra.palavraEscolhida.toCharArray();
+        char[] palavraArray = Main.palavra.palavrasEscolhidas[qualPalavraDica-1].toCharArray();
 
         System.out.printf("Primeira letra: %s\nÚltima letra: %s\n", palavraArray[0],palavraArray[palavraArray.length-1]);
     }
