9e1b5e60314778fa02cbc14dce25282fe211e870
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-30 09:16:32 -0300

    Update Anagrama.java

--- Jogos 2.0/Anagrama.java
+++ Jogos 2.0/Anagrama.java
@@ -1,31 +1,39 @@
 public class Anagrama {
     Global global = new Global();
-    String palavraEmbaralhada;
+    String[] palavrasEmbaralhadas;
 
     public void jogo() {
-        Main.palavra.palavraEscolhida = Main.palavra.sorteiaPalavra(Main.palavra.vetorPalavras);
+        Main.palavra.sorteiaPalavra(Main.palavra.vetorPalavras);
 
-        palavraEmbaralhada = embaralhaPalavra(Main.palavra.getPalavraEscolhida());
+        palavrasEmbaralhadas = embaralhaPalavras(Main.palavra.palavrasEscolhidas);
 
-        System.out.println(palavraEmbaralhada);
+        System.out.println(palavrasEmbaralhadas[0]);
+        System.out.println(palavrasEmbaralhadas[1]);
+
+        Main.palavra.palavraReset();
 
         Main.palavra.tentativa();
 
     }
 
-    private String embaralhaPalavra(String palavra) {
-        char[] palavraArray = palavra.toCharArray();
+    private String[] embaralhaPalavras(String[] palavrasEscolhidas) {
+        String[] novasPalavras = new String[2];
+        for (int iPalavra = 0; iPalavra < palavrasEscolhidas.length; iPalavra++) {
+            char[] palavraArray = palavrasEscolhidas[iPalavra].toCharArray();
 
-        for (int i = 0; i < palavraArray.length; i++) {
-            int posicaoAleatoria = global.inteiroAleatorio(0, palavraArray.length);
+            for (int i = 0; i < palavraArray.length; i++) {
+                int posicaoAleatoria = global.inteiroAleatorio(0, palavraArray.length);
 
-            char aux = palavraArray[i];
-            palavraArray[i] = palavraArray[posicaoAleatoria];
-            palavraArray[posicaoAleatoria] = aux;
+                char aux = palavraArray[i];
+                palavraArray[i] = palavraArray[posicaoAleatoria];
+                palavraArray[posicaoAleatoria] = aux;
+            }
+
+            novasPalavras[iPalavra] = new String(palavraArray);
+
         }
 
-
-        return new String(palavraArray);
+        return novasPalavras;
     }
 
 }
