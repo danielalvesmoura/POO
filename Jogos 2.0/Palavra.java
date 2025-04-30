e671afca3c798b94c7364d9816e401108bf1107e
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-30 09:16:08 -0300

    Update Palavra.java

--- Jogos 2.0/Palavra.java
+++ Jogos 2.0/Palavra.java
@@ -1,40 +1,61 @@
+import java.util.Scanner;
+
 public class Palavra {
 
     Global global = new Global();
 
     int dificuldade = 4;
-    String[] vetorPalavras = {"salve","casa","trilho"};
-    String palavraEscolhida;
+    String[] vetorPalavras = {"salve","casa","faca","gato","trilho"};
+    String[] palavrasEscolhidas = new String[2];
     String resposta;
+    Boolean[] palavrasAcertadas = {false,false};
+    int acertouQuantas = 0;
 
-    public String sorteiaPalavra(String[] vetorPalavras) {
-        String palavraEscolhida = "";
-        while(palavraEscolhida.length() != dificuldade) {
-            palavraEscolhida = vetorPalavras[global.inteiroAleatorio(0, vetorPalavras.length)];
+    public void palavraReset() {
+        acertouQuantas = 0;
+        resposta = "";
+        palavrasAcertadas[0] = false;
+        palavrasAcertadas[1] = false;
+    }
+
+    public void sorteiaPalavra(String[] vetorPalavras) {
+        for (int i = 0; i < palavrasEscolhidas.length; i++) {
+            palavrasEscolhidas[i] = "";
+            while(palavrasEscolhidas[i].length() != dificuldade) {
+                palavrasEscolhidas[i] = vetorPalavras[global.inteiroAleatorio(0, vetorPalavras.length)];
+            }
         }
-
-        return palavraEscolhida;
     }
 
     public void tentativa() {
-        Boolean acertou = false;
-        while (!acertou) {
+        while (acertouQuantas != 2) {
+            Boolean jaFoi = false;
+            while(jaFoi != false)
+            System.out.println("Qual deseja acertar? [1] primeira ou [2] segunda");
+            int qualPalavra = global.scanner.nextInt();
 
-            resposta = global.scanner.nextLine();
+            while (palavrasAcertadas[qualPalavra-1] == false) {
 
-            acertou = checaResposta();
+                global.scanner = new Scanner(System.in);
+                resposta = global.scanner.nextLine();
+
+                palavrasAcertadas[qualPalavra-1] = checaResposta(qualPalavra);
+            }
         }
     }
 
-    public Boolean checaResposta() {
-        if (resposta.equals(palavraEscolhida)) {
+    public Boolean checaResposta(int qualPalavra) {
+        if (resposta.equals(palavrasEscolhidas[qualPalavra-1])) {
+            acertouQuantas++;
             System.out.println("\nVocê acertou!");
             System.out.println("Você ganhou 1 ponto!\n");
             Main.pontuacao.atualizaPontos();
             System.out.printf("Sua pontuação atual é %d ponto(s)\n", Main.pontuacao.pontos);
             return true;
         } else if (resposta.equals("dica")) {
-            Main.dica.dica();
+            System.out.println("De qual palavra deseja dica? [1] primeira ou [2] segunda");
+            int qualPalavraDica = global.scanner.nextInt();
+            Main.dica.dica(qualPalavraDica);
             return false;
         } else if (resposta.equals("desisto")) {
             return true;
@@ -48,13 +69,6 @@ public class Palavra {
         this.dificuldade = dificuldade;
     }
 
-    public void setPalavraEscolhida(String palavraEscolhida) {
-        this.palavraEscolhida = palavraEscolhida;
-    }
-
-    public String getPalavraEscolhida() {
-        return palavraEscolhida;
-    }
 
     public int getDificuldade() {
         return dificuldade;
