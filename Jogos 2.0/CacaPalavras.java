3dafeecd1a451e9dc514f65cca97cea9c1a52f36
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-30 20:46:57 -0300

    Update CacaPalavras.java

--- Jogos 2.0/CacaPalavras.java
+++ Jogos 2.0/CacaPalavras.java
@@ -7,66 +7,66 @@ public class CacaPalavras {
 
     int tamanhoTabuleiro;
 
-    public void jogo() {
+    public void jogo(Palavra palavra, Pontuacao pontuacao, Dica dica) {
 
-        tamanhoTabuleiro = Main.palavra.getDificuldade()+2;
+        tamanhoTabuleiro = palavra.getDificuldade()+2;
 
-        Main.palavra.sorteiaPalavra(Main.palavra.vetorPalavras);
+        palavra.sorteiaPalavra(palavra.vetorPalavras);
 
         char[][] tabuleiro1 = tabuleiro.gerarTabuleiro(tamanhoTabuleiro);
 
         int orientacaoPalavra = global.inteiroAleatorio(0, 2);  // Decide se a palavra estará na vertical ou horizontal
 
-        int[] posicaoYPalavras = tabuleiro.geraPosicaoYPalavras(tamanhoTabuleiro, Main.palavra.palavrasEscolhidas, orientacaoPalavra);
-        int[] posicaoXPalavras = tabuleiro.geraPosicaoXPalavras(tamanhoTabuleiro, Main.palavra.palavrasEscolhidas, orientacaoPalavra);
+        int[] posicaoYPalavras = tabuleiro.geraPosicaoYPalavras(tamanhoTabuleiro, palavra.palavrasEscolhidas, orientacaoPalavra);
+        int[] posicaoXPalavras = tabuleiro.geraPosicaoXPalavras(tamanhoTabuleiro, palavra.palavrasEscolhidas, orientacaoPalavra);
 
-        tabuleiro1 = tabuleiro.ocultarPalavraTabuleiro(tabuleiro1,Main.palavra.palavrasEscolhidas,posicaoXPalavras, posicaoYPalavras, orientacaoPalavra);
+        tabuleiro1 = tabuleiro.ocultarPalavraTabuleiro(tabuleiro1,palavra.palavrasEscolhidas,posicaoXPalavras, posicaoYPalavras, orientacaoPalavra);
 
         tabuleiro.mostrarTabuleiro(tabuleiro1);
 
-        tentativa();
+        tentativa(palavra, pontuacao, dica);
 
     }
 
-    private void tentativa() {
-        Main.palavra.acertouQuantas = 0;
-        while (Main.palavra.acertouQuantas != 2) {
+    private void tentativa(Palavra palavra, Pontuacao pontuacao, Dica dica) {
+        palavra.acertouQuantas = 0;
+        while (palavra.acertouQuantas != 2) {
             System.out.println("Digiter a palavra que encontrar");
 
             global.scanner = new Scanner(System.in);
-            Main.palavra.resposta = global.scanner.nextLine();
+            palavra.resposta = global.scanner.nextLine();
 
-            checaResposta();
+            checaResposta(palavra, pontuacao, dica);
         }
     }
 
-    private void checaResposta() {
+    private void checaResposta(Palavra palavra, Pontuacao pontuacao, Dica dica) {
         Boolean encontrou = false;
 
-        for (int i = 0; i < Main.palavra.palavrasEscolhidas.length; i++) {
-            if (Main.palavra.resposta.equals(Main.palavra.palavrasEscolhidas[i])) {
+        for (int i = 0; i < palavra.palavrasEscolhidas.length; i++) {
+            if (palavra.resposta.equals(palavra.palavrasEscolhidas[i])) {
                 encontrou = true;
-                Main.palavra.palavrasAcertadas[i] = true;
+                palavra.palavrasAcertadas[i] = true;
             }
         }
 
         if (encontrou) {
 
-            Main.palavra.acertouQuantas++;
+            palavra.acertouQuantas++;
 
             System.out.println("\nVocê acertou uma palavra!");
             System.out.println("Você ganhou 1 ponto!\n");
-            Main.pontuacao.atualizaPontos();
-            System.out.printf("Sua pontuação atual é %d ponto(s)\n", Main.pontuacao.pontos);
+            pontuacao.atualizaPontos();
+            System.out.printf("Sua pontuação atual é %d ponto(s)\n", pontuacao.pontos);
 
-        } else if (Main.palavra.resposta.equals("dica")) {
+        } else if (palavra.resposta.equals("dica")) {
 
             System.out.println("De qual palavra deseja dica? [1] primeira ou [2] segunda");
             int qualPalavraDica = global.scanner.nextInt();
-            Main.dica.dica(qualPalavraDica);
+            dica.dica(qualPalavraDica, palavra);
 
-        } else if (Main.palavra.resposta.equals("desisto")) {
-            Main.palavra.acertouQuantas = 2;
+        } else if (palavra.resposta.equals("desisto")) {
+            palavra.acertouQuantas = 2;
 
         } else {
             System.out.println("Seu horrível! Tente novamente:\n");
