8b35f1d2fd3484ab6e048bceeda833d77d9fe5b6
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-30 22:06:20 -0300

    Update CacaPalavras.java

--- Jogos 2.0/CacaPalavras.java
+++ Jogos 2.0/CacaPalavras.java
@@ -1,34 +1,49 @@
 import java.util.Scanner;
 
 public class CacaPalavras {
-
+    Palavra palavra;
     Global global = new Global();
     Tabuleiro tabuleiro = new Tabuleiro();
-
+    String[] palavrasEscolhidas;
     int tamanhoTabuleiro;
 
-    public void jogo(Palavra palavra, Pontuacao pontuacao, Dica dica) {
+    public void setPalavra(Palavra palavra) {
+        this.palavra = palavra;
+    }
 
-        tamanhoTabuleiro = palavra.getDificuldade()+2;
+    public void setPalavrasEscolhidas(String[] palavrasEscolhidas) {
+        this.palavrasEscolhidas = palavrasEscolhidas;
+    }
 
-        palavra.sorteiaPalavra(palavra.vetorPalavras);
+    public void setTamanhoTabuleiro(int tamanhoTabuleiro) {
+        this.tamanhoTabuleiro = tamanhoTabuleiro;
+    }
+
+    public CacaPalavras(Palavra palavra){
+        setPalavra(palavra);
+        setPalavrasEscolhidas(palavra.palavrasEscolhidas);
+        setTamanhoTabuleiro(palavra.dificuldade+2);
+    }
+
+    public void jogo() {
+        palavra.sorteiaPalavra(palavra.getVetorPalavras());
 
         char[][] tabuleiro1 = tabuleiro.gerarTabuleiro(tamanhoTabuleiro);
 
         int orientacaoPalavra = global.inteiroAleatorio(0, 2);  // Decide se a palavra estará na vertical ou horizontal
 
-        int[] posicaoYPalavras = tabuleiro.geraPosicaoYPalavras(tamanhoTabuleiro, palavra.palavrasEscolhidas, orientacaoPalavra);
-        int[] posicaoXPalavras = tabuleiro.geraPosicaoXPalavras(tamanhoTabuleiro, palavra.palavrasEscolhidas, orientacaoPalavra);
+        int[] posicaoYPalavras = tabuleiro.geraPosicaoYPalavras(tamanhoTabuleiro, palavrasEscolhidas, orientacaoPalavra);
+        int[] posicaoXPalavras = tabuleiro.geraPosicaoXPalavras(tamanhoTabuleiro, palavrasEscolhidas, orientacaoPalavra);
 
-        tabuleiro1 = tabuleiro.ocultarPalavraTabuleiro(tabuleiro1,palavra.palavrasEscolhidas,posicaoXPalavras, posicaoYPalavras, orientacaoPalavra);
+        tabuleiro1 = tabuleiro.ocultarPalavraTabuleiro(tabuleiro1,palavrasEscolhidas,posicaoXPalavras, posicaoYPalavras, orientacaoPalavra);
 
         tabuleiro.mostrarTabuleiro(tabuleiro1);
 
-        tentativa(palavra, pontuacao, dica);
+        tentativa(palavra);
 
     }
 
-    private void tentativa(Palavra palavra, Pontuacao pontuacao, Dica dica) {
+    private void tentativa(Palavra palavra) {
         palavra.acertouQuantas = 0;
         while (palavra.acertouQuantas != 2) {
             System.out.println("Digiter a palavra que encontrar");
@@ -36,15 +51,15 @@ public class CacaPalavras {
             global.scanner = new Scanner(System.in);
             palavra.resposta = global.scanner.nextLine();
 
-            checaResposta(palavra, pontuacao, dica);
+            checaResposta(palavra);
         }
     }
 
-    private void checaResposta(Palavra palavra, Pontuacao pontuacao, Dica dica) {
-        Boolean encontrou = false;
+    private void checaResposta(Palavra palavra) {
+        boolean encontrou = false;
 
-        for (int i = 0; i < palavra.palavrasEscolhidas.length; i++) {
-            if (palavra.resposta.equals(palavra.palavrasEscolhidas[i])) {
+        for (int i = 0; i < palavrasEscolhidas.length; i++) {
+            if (palavra.resposta.equals(palavrasEscolhidas[i])) {
                 encontrou = true;
                 palavra.palavrasAcertadas[i] = true;
             }
@@ -56,14 +71,14 @@ public class CacaPalavras {
 
             System.out.println("\nVocê acertou uma palavra!");
             System.out.println("Você ganhou 1 ponto!\n");
-            pontuacao.atualizaPontos();
-            System.out.printf("Sua pontuação atual é %d ponto(s)\n", pontuacao.pontos);
+            palavra.atualizaPontos();
+            System.out.printf("Sua pontuação atual é %d ponto(s)\n", palavra.pontos);
 
         } else if (palavra.resposta.equals("dica")) {
 
             System.out.println("De qual palavra deseja dica? [1] primeira ou [2] segunda");
             int qualPalavraDica = global.scanner.nextInt();
-            dica.dica(qualPalavraDica, palavra);
+            palavra.dica(qualPalavraDica, palavra);
 
         } else if (palavra.resposta.equals("desisto")) {
             palavra.acertouQuantas = 2;
