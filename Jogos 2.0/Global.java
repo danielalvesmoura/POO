6ea2b30cac6cf28c6f8c55e45752938ef0b1b50a
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-04-30 22:06:10 -0300

    Update Global.java

--- Jogos 2.0/Global.java
+++ Jogos 2.0/Global.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Global {
 
-    Random random = new Random();
+    static Random random = new Random();
     Scanner scanner = new Scanner(System.in);
 
-    public int inteiroAleatorio(int minimo, int maximo) {
+    public static int inteiroAleatorio(int minimo, int maximo) {
         return random.nextInt(maximo-minimo) + minimo;
     }
 
